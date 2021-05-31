a3c207861e622ffdcc9f50eeb63edf518d2c28d2
Clownie <darrenbell2k34@gmail.com>
2021-05-31 22:37:19 +0100

    Innitial setup complete imported nuget for monogame directx setup the game class.

---
+++ FirstGameProject/Game1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstGameProject
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager _graphics;
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager( this );
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+        protected override void Update( GameTime gameTime )
+        {
+            base.Update( gameTime );
+        }
+        protected override void Draw( GameTime gameTime )
+        {
+            GraphicsDevice.Clear( Color.Chocolate );
+
+            base.Draw( gameTime );
+        }
+    }
+}
