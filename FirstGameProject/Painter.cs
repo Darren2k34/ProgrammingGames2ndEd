d384e7db8ebeb7a75293914048991e707798d69e
Clownie <darrenbell2k34@gmail.com>
2021-06-07 14:52:52 +0100

    removed commented code

--- FirstGameProject/Painter.cs
+++ FirstGameProject/Painter.cs
@@ -27,24 +27,7 @@ namespace FirstGameProject
         private MouseState _currentMouseState, _previousMouseState;
         private KeyboardState _currentKeyboardState, _previousKeyboardState;
 
-
         private CannonBall currentCannonBall;
-        //private Texture2D _balloon;
-        //private Vector2 _balloonPosition;
-        //private Vector2 _balloonOrigin;
-
-        //private Texture2D _cannonBarrel;
-        //private Texture2D _colourRed;
-        //private Texture2D _colourGreen;
-        //private Texture2D _colourBlue;
-
-        //private Vector2 _colourOrigin;
-
-        //private Vector2 _cannonBarrelPosition;
-        //private Vector2 _cannonBarrelOrigin;
-        //private float _cannonBarrelAngle;
-
-
 
         public Painter( GraphicsDevice graphicsDevice, SpriteBatch spriteBatch, ContentManager contentManager )
         {
@@ -56,20 +39,6 @@ namespace FirstGameProject
         {
             MediaPlayer.Play( Content.Load<Song>( "snd_music" ) );
             _background = Content.Load<Texture2D>( "spr_background" );
-            //_balloon = Content.Load<Texture2D>( "spr_lives" );
-            //_cannonBarrel = Content.Load<Texture2D>( "spr_cannon_barrel" );
-            //_colourRed = Content.Load<Texture2D>( "spr_cannon_red" );
-            //_colourGreen = Content.Load<Texture2D>( "spr_cannon_green" );
-            //_colourBlue = Content.Load<Texture2D>( "spr_cannon_blue" );
-
-            //_balloonPosition = Vector2.Zero;
-            //_balloonOrigin = new Vector2( _balloon.Width * 0.5f, _balloon.Height /* * 0.5f */ );
-
-            //_cannonBarrelPosition = new Vector2( 72, 405 );
-            //_cannonBarrelOrigin = new Vector2( _cannonBarrel.Height * 0.5f, _cannonBarrel.Height * 0.5f );
-
-            //_colourOrigin = new Vector2( _colourRed.Width * 0.5f, _colourRed.Height * 0.5f );
-            //_currentColour = Color.Blue;
 
             var cannonBalls = new Dictionary<BallColour, CannonBall>( new KeyValuePair<BallColour, CannonBall>[] {
                     new KeyValuePair<BallColour, CannonBall>( BallColour.Red, new CannonBall( Content.Load<Texture2D>( "spr_cannon_red" ) )),
@@ -81,11 +50,6 @@ namespace FirstGameProject
 
         public void Update( GameTime gameTime )
         {
-            /* var yPosition = 480 - gameTime.TotalGameTime.Milliseconds / 2;
-             * var xPosition = gameTime.TotalGameTime.Milliseconds / 2;
-             * _balloonPosition = new Vector2(xPosition, yPosition );
-            */
-
             HandleInput();
 
             if ( _currentColour == BallColour.Red )
@@ -101,7 +65,6 @@ namespace FirstGameProject
 
             _spriteBatch.Begin();
             _spriteBatch.Draw( _background, Vector2.Zero, Color.White );
-            //_spriteBatch.Draw( _balloon, _balloonPosition, null, Color.White, 0.0f, _balloonOrigin, 1.0f, SpriteEffects.None, 0 );
             _spriteBatch.Draw( _cannon.Texture, _cannon.Position, null, Color.White, _cannon.Angle, _cannon.Origin, 1.0f, SpriteEffects.None, 0 );
             _spriteBatch.Draw( currentCannonBall.Texture, currentCannonBall.Position, null, Color.White, 0f, currentCannonBall.Origin, 1.0f, SpriteEffects.None, 0 );
             _spriteBatch.End();
@@ -139,8 +102,6 @@ namespace FirstGameProject
             } else {
                 _cannon.Angle = 0;
             }
-
-            //_balloonPosition = new Vector2( _currentMouseState.X, _currentMouseState.Y );
 
             _previousMouseState = _currentMouseState;
             _previousKeyboardState = _currentKeyboardState;
